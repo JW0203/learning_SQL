011560cae5e5a9ef58e3521bcd22b73ee25a4961
JW0203 <widwoo0203@gmail.com>
2023-09-14 15:21:36 -0700

    feat: 없어진 데이터찾기

---
+++ programers_JOIN_06.sql
@@ -0,0 +1,8 @@
+-- 없어진 기록찾기
+/* 천재지변으로 인해 일부 데이터가 유실되었습니다. 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+*/
+SELECT O.ANIMAL_ID, O.NAME 
+FROM ANIMAL_OUTS O
+    LEFT JOIN ANIMAL_INS I
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+    WHERE O.ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM ANIMAL_INS)
